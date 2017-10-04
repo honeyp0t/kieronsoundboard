0a8ea02d71fcecb4b1f1960d5746ac2eaf71f8c7
honeyp0t <robin.lindell@gmail.com>
2017-10-04 19:38:58 +0200

    init

--- App.js
+++ App.js
@@ -1,23 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableHighlight, StyleSheet, Image, Text, View } from 'react-native';
+import Header from './components/header';
+//import Sound from 'react-native-sound';
 
 export default class App extends React.Component {
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+          <Header />
+          <View style={styles.container}>
+              <View style={styles.header}>
+                  <Image source={require('./resources/img/header.png')} style={styles.headerimg} resizeMode="contain" />
+              </View>
+              {this.newRow()}
+              {this.newRow()}
+              {this.newRow()}
+              {this.newRow()}
+          </View>
       </View>
     );
   }
+
+  newRow() {
+	return (
+        <View style={styles.imagerow}>
+		  {this.newImage('dreamer.jpg')}
+		  {this.newImage('freakout.jpg')}
+		  {this.newImage('hush.jpeg')}
+		</View>
+	);
+  }
+
+  newImage(imgname) {
+//      var clip = new Sound(require('./frog.wav'));
+
+	return (
+      <TouchableHighlight style={styles.touchable} onPress={this.playSound}>
+          <Image source={require('./resources/img/lul.png')} style={styles.img} resizeMode="contain"/>
+      </TouchableHighlight>
+	);
+  }
+
+  playSound() {
+      //clip.play();
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+//    flex: 1,
+//    flexDirection: 'column',
+    height: 500,
+    backgroundColor: '#aaaaff',
     alignItems: 'center',
-    justifyContent: 'center',
+//    paddingTop: 10, // TODO bör vara storlek på telefonuits topram, gör till komponennt istället
+	//height:
+ //   justifyContent: 'space-around',
+  },
+  header: {
+    flex: 1.5,
+    flexDirection: 'row',
+    backgroundColor: '#ddccee',
+  },
+  headerimg: {
+      flex: 1,
+  },
+  imagerow: {
+    flex: 1,
+	flexDirection: 'row',
+//	justifyContent: 'space-around',
+	backgroundColor: '#bbccaa',
+  },
+  img: {
+  	flex: 1,
+	width: "100%",
+	height: "100%",
+	backgroundColor: '#bbaabb',
+	//flexDirection: 'column',
+	resizeMode: 'contain',
+	margin: 4,
+  },
+  touchable: {
+      flex: 1,
+      width: '33%',
+      height: '100%',
   },
 });
---
+++ components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function header() {
+    return (
+      <View style={{height: 25, backgroundColor: '#000000'}}></View>
+    );
+}
